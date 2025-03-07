0260ac1ea9a51662f314809a490f5938dd96a14f
Kyrelle <kyrelleandre@gmail.com>
2025-03-07 13:22:41 +0800

    Create Pages and Routes to initialize different pages in the website such as Profile,  SignIn, Signup, and ETC.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
 import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Stock from "./pages/Stock";
+import ForgotPassword from "./pages/ForgotPassword";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Hello Brah</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/stock" element={<Stock />} />
+        </Routes>
+      </Router>
     </>
   )
 }
