98d9ec59b1d753515d9a569e066827ca819fa4e0
Kyrelle <kyrelleandre@gmail.com>
2025-03-21 14:30:08 +0800

    Complete the OAuth functionality for the Connect with Google button with pop-up sign in

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -1,16 +1,53 @@
-import React from 'react'
+import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import { doc, getDoc, setDoc } from "firebase/firestore";
+import { serverTimestamp } from "firebase/firestore";
+import React from "react";
 
 //Import Google Icon from https://react-icons.github.io/react-icons
 import { FcGoogle } from "react-icons/fc";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
+import { db } from "../firebase";
 
 export default function OAuth() {
+  const navigate = useNavigate();
+
+  async function onGoogleClick() {
+    try {
+      const auth = getAuth();
+      const provider = new GoogleAuthProvider();
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+
+      // check for the user
+
+      const docRef = doc(db, "users", user.uid);
+      const docSnap = await getDoc(docRef);
+      if (!docSnap.exists()) {
+        await setDoc(docRef, {
+          name: user.displayName,
+          email: user.email,
+          timestamp: serverTimestamp(),
+        });
+      }
+      navigate("/");
+    } catch (error) {
+      toast.error("Could not authorize with Google");
+      console.log(error);
+    }
+  }
+
   return (
-    <button className='flex items-center justify-center w-full bg-red-600 
+    <button
+      type="button"
+      onClick={onGoogleClick}
+      className="flex items-center justify-center w-full bg-red-600 
     text-white px-7 py-[0.7rem] rounded uppercase text-sm font-medium 
     hover:bg-red-800 active:bg-red-900 shadow-md hover:shadow-lg
-    transition duration-300 ease-in-out'>
-        <FcGoogle className='text-2xl bg-white rounded-full mr-2'/>
-        Continue with Google
+    transition duration-300 ease-in-out"
+    >
+      <FcGoogle className="text-2xl bg-white rounded-full mr-2" />
+      Continue with Google
     </button>
-  )
+  );
 }
